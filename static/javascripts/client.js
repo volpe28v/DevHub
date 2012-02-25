e6b17d2b6d0c00de17889735e1bda04c46c2bdf7
naoki kodama <naoki.koda@gmail.com>
2012-02-25 21:56:19 +0900

    未使用関数を削除

--- static/javascripts/client.js
+++ static/javascripts/client.js
@@ -157,7 +157,6 @@ function init_websocket(){
         }
       }())
 
-
       var log_dd = $("<dd/>")
       var log_pre = $("<pre/>").text(text_logs[i].text)
 
@@ -199,17 +198,6 @@ function suggest_start(list){
     suggest_obj.candidateList = suggest_list;
   }
 }
-
-function getFullDate(date){
-  var yy = date.getYear();
-  var mm = date.getMonth() + 1;
-  var dd = date.getDate();
-  if (yy < 2000) { yy += 1900; }
-  if (mm < 10) { mm = "0" + mm; }
-  if (dd < 10) { dd = "0" + dd; }
-
-  return yy + '/' + mm + '/' + dd + ' ' + date.toLocaleTimeString();
-};
 
 function append_msg(data){
   //TODO: System メッセージを非表示にする。
