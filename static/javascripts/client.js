1b27baf525f7e04e1dac6f1cd8a4f0f204a150f8
naoki kodama <naoki.koda@gmail.com>
2012-05-30 23:28:59 +0900

    Fix incorrect image link.

--- static/javascripts/client.js
+++ static/javascripts/client.js
@@ -328,7 +328,7 @@ function decorate_link_tag( text ){
   var linked_text = text.replace(/((https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+))/g,
       function(){
         var matched_link = arguments[1];
-        if ( matched_link.match(/[.jpg|.gif|.png]$/)){
+        if ( matched_link.match(/(\.jpg|\.gif|\.png|\.bmp)$/)){
           return '<img src="' + matched_link + '"/>';
         }else{
           return '<a href="' + matched_link + '" target="_blank" >' + matched_link + '</a>';
