dfe1be81885ee68750e8c117bf5e0080257243e0
naoki kodama <naoki.koda@gmail.com>
2014-02-27 22:58:09 +0900

    ドロップ画層のファイル名をBASE64エンコードをやめて、スペースを_に置き換えるようにした。

--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,9 @@
 var fs = require('fs');
 exports.post = function(req, res) {
   var tmp_path = req.files.file.path;
-  var file_name = req.files.file.name;
-  var enc_name = new Buffer(file_name).toString('base64') + "." + file_name.split(".").pop();
-  var target_path = './static/uploads/' + enc_name;
-  var access_path = '/uploads/' + enc_name;
+  var file_name = req.files.file.name.replace(/ /g,'_');
+  var target_path = './static/uploads/' + file_name;
+  var access_path = '/uploads/' + file_name;
 
   fs.rename(tmp_path, target_path, function(err) {
     if (err) {
