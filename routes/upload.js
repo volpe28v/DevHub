110a07a430224704af9095883f126efadbd2052b
naoki kodama <naoki.koda@gmail.com>
2014-03-02 20:59:34 +0900

    fix #190. アップロードしたファイル名に日付を付加するようにした

--- routes/upload.js
+++ routes/upload.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
+var util = require('../lib/util');
+
 exports.post = function(req, res) {
   var tmp_path = req.files.file.path;
-  var file_name = req.files.file.name.replace(/[ 　]/g,'_'); // 半角・全角スペースを置き換え
+  var file_name = util.getFullDateNoSepa(new Date()) + '_' + req.files.file.name.replace(/[ 　]/g,'_'); // 半角・全角スペースを置き換え
   var target_path = './static/uploads/' + file_name;
   var access_path = '/uploads/' + file_name;
 
