7db207e6d7b86c56200e0dd7a3838824f8b70a7b
naoki kodama <naoki.koda@gmail.com>
2014-03-09 16:56:03 +0900

    refactoring. チャット関連の処理を ChatController に分割中

---
+++ static/javascripts/libs/chat_controller.js
@@ -0,0 +1,346 @@
+function ChatController(param){
+  this.socket = param.socket;
+  this.faviconNumber = param.faviconNumber;
+
+  this.latest_login_list = [];
+  var that = this;
+  $('#list').on('click', '.remove_msg', function(){
+    var id = "#" + $(this).closest('li').attr('id');
+    var data_id = $(this).closest('li').data('id');
+    $(id).fadeOut();
+    that.send_remove_msg(data_id);
+  });
+
+  $('#list').on('click','.login-name-base', function(){
+    var name = $(this).text();
+    $('#message').val($('#message').val() + " @" + name + "さん ");
+    $('#message').focus();
+  });
+
+  this.init_notification();
+}
+
+ChatController.prototype = {
+  // public
+  bindSocketEvent: function(){
+    var that = this;                    
+    this.socket.on('message_own', function(data) {
+      var $msg = that.prepend_own_msg(data);
+      that.setColorbox($msg.find('.thumbnail'));
+      that.faviconNumber.up();
+    });
+
+    this.socket.on('message', function(data) {
+      var $msg = that.prepend_msg(data);
+      that.setColorbox($msg.find('.thumbnail'));
+      that.do_notification(data);
+      that.faviconNumber.up();
+    });
+
+    this.socket.on('remove_message', function(data) {
+      $('#msg_' + data.id).fadeOut();
+    });
+
+    this.socket.on('list', function(login_list) {
+      $('#login_list_loader').hide();
+
+      var out_list = "";
+      for (var i = 0; i < login_list.length; ++i){
+        var login_elem = "";
+        var place = "";
+        if ( login_list[i].place != "" ){
+          place = "@" + login_list[i].place;
+        }
+        if ( login_list[i].pomo_min > 0 ){
+          login_elem = '<span class="login-elem login-name-pomo"><span class="name">' + login_list[i].name + '</span>' + place + ' <span class="pomo-min">' + login_list[i].pomo_min + 'min</span></span>'
+        }else{
+          login_elem = '<span class="login-elem login-name' + that.get_color_id_by_name_id(login_list[i].id) + '"><span class="name">' + login_list[i].name + '</span>' + place + '</span>'
+        }
+        out_list += login_elem + "<wbr>";
+      }
+      out_list = "<nobr>" + out_list + "</nobr>";
+
+      if ($('#login_list').html() != out_list){
+        $('#login_list').html(out_list);
+        $('#login_list').fadeIn();
+        that.suggest_start(login_list);
+
+        // add click event for each login names.
+        $('#login_list .login-elem').click(function(){
+          var name = $(this).children(".name").text();
+          $('#message').val($('#message').val() + " @" + name + "さん ");
+          $('#message').focus();
+        });
+      }
+
+      this.latest_login_list = login_list.sort(function(a,b){ return b.name.length - a.name.length });
+    });
+
+    this.socket.on('latest_log', function(msgs) {
+      for ( var i = 0 ; i < msgs.length; i++){
+        that.append_msg(msgs[i])
+      }
+
+      setColorbox($('#chat_body').find('.thumbnail'));
+    });
+
+    $('#form').submit(function() {
+      var name = $('#name').val();
+      var message = $('#message').val();
+      $.cookie(COOKIE_NAME,name,{ expires: COOKIE_EXPIRES });
+
+      if ( message && name ){
+        login_name = name;
+        socket.emit('message', {name:name, msg:message});
+        $('#message').attr('value', '');
+      }
+      return false;
+    });
+  },
+
+  // private
+  suggest_start: function(list){
+    var suggest_list = []
+      for (var i = 0; i < list.length; ++i){
+        suggest_list.push("@" + list[i].name + "さん");
+      }
+
+    if (this.suggest_obj == undefined){
+      this.suggest_obj = new Suggest.LocalMulti("message", "suggest", suggest_list, {interval: 200, dispAllKey: false, prefix: true, highlight: true});
+    }else{
+      this.suggest_obj.candidateList = suggest_list;
+    }
+  },
+
+  append_msg: function(data){
+    //TODO: System メッセージを非表示にする。
+    //      切り替え可能にするかは検討する。
+    if (data.name == "System") { return };
+    if (this.exist_msg(data)){ return };
+
+    var msg = this.get_msg_html(data);
+
+    $('#list').append(msg.li.addClass(msg.css));
+    msg.li.fadeIn();
+  },
+
+  prepend_msg: function(data){
+    //TODO: System メッセージを非表示にする。
+    //      切り替え可能にするかは検討する。
+    if (data.name == "System") { return }
+    if (this.exist_msg(data)){ return };
+
+    var msg = this.get_msg_html(data);
+
+    $('#list').prepend(msg.li);
+    msg.li.addClass("text-highlight",0);
+    msg.li.slideDown('fast',function(){
+      msg.li.switchClass("text-highlight", msg.css, 500);
+    });
+
+    return msg.li;
+  },
+
+  prepend_own_msg: function(data){
+    if (this.exist_msg(data)){ return };
+    var msg = this.get_msg_html(data);
+
+    $('#list').prepend(msg.li);
+    msg.li.addClass("text-highlight",0);
+    msg.li.slideDown('fast',function(){
+      msg.li.switchClass("text-highlight", msg.css, 500);
+    });
+
+    return msg.li;
+  },
+
+  exist_msg: function(data){
+    if (data.msg == undefined) { data.msg = ""; }
+    var id = '#msg_' + data._id.toString();
+    return $(id).size() > 0;
+  },
+
+  get_msg_html: function(data){
+    if ( data.name == login_name ){
+      return {
+        li: this.get_msg_li_html(data).html(this.get_msg_body(data) + '<a class="remove_msg">x</a><span class="own_msg_date">' + data.date + '</span></td></tr></table>'),
+        css: "own_msg"
+      };
+    } else if (this.include_target_name(data.msg,login_name)){
+      return {
+        li: this.get_msg_li_html(data).html(this.get_msg_body(data) + ' <span class="target_msg_date">' + data.date + '</span></td></tr></table>'),
+          css: "target_msg"
+      };
+    }else{
+      return {
+        li: this.get_msg_li_html(data).html(this.get_msg_body(data) + ' <span class="date">' + data.date + '</span></td></tr></table>'),
+          css: null
+      };
+    }
+  },
+
+  get_msg_li_html: function(data){
+    if ( data._id != undefined ){
+      return $('<li/>').attr('style','display:none').attr('id','msg_' + data._id.toString()).attr('data-id', data._id.toString());
+    }else{
+      return $('<li/>').attr('style','display:none');
+    }
+  },
+
+  get_msg_body: function(data){
+    var date = new Date();
+    var id = date.getTime();
+
+    var name_class = "login-name";
+    var msg_class = "msg";
+
+    data.id = this.get_id(data.name)
+
+    if ( data.name == "System" ){
+      name_class = "login-name-system";
+      msg_class = "msg_ext"
+    }else if ( data.ext == true ){
+      name_class = "login-name-ext";
+      msg_class = "msg_ext"
+    }else if ( data.name == "Pomo" ){
+      name_class = "login-name-pomosys";
+      msg_class = "msg_pomo"
+    }else{
+      name_class = "login-name" + this.get_color_id_by_name_id(data.id);
+    }
+
+    return '<table><tr><td nowrap valign="top"><span class="login-name-base ' + name_class + '">' + data.name + '</span></td><td width="100%"><span class="msg_text ' + msg_class + '">' + this.decorate_msg(data.msg) + '</span>';
+  },
+
+  get_color_id_by_name_id: function(id){
+    if(id == 0){ return 0; } // no exist user.
+    return id % LOGIN_COLOR_MAX + 1; // return 1 〜 LOGIN_COLOR_MAX
+  },
+
+  decorate_msg: function(msg){
+    var deco_msg = msg;
+
+    deco_msg = this.deco_login_name(deco_msg)
+    deco_msg = $.decora.message_to_html(deco_msg);
+
+    return deco_msg;
+  },
+
+  deco_login_name: function(msg){
+    var deco_msg = msg;
+    var name_reg = RegExp("@(.+?)さん", "g");
+    deco_msg = deco_msg.replace( name_reg, function(){
+      if (arguments[1] == login_name){
+        return '<span class="label label-important">' + arguments[0] + '</span>'
+      }else{
+        return '<span class="label label-info">' + arguments[0] + '</span>'
+      }
+    });
+    return deco_msg;
+  },
+
+  include_target_name: function(msg,name){
+    var name_reg = RegExp("@" + name + "( |　|さん|$)");
+    if (msg.match(name_reg)){
+      return true;
+    }
+    return false;
+  },
+
+  get_id: function(name){
+    for(var i = 0; i < this.latest_login_list.length; ++i ){
+      if ( this.latest_login_list[i].name == name ){
+        return this.latest_login_list[i].id;
+      }
+    }
+    return 0;
+  },
+
+  send_remove_msg: function(id){
+    this.socket.emit('remove_message', {id:id});
+  },
+
+  setColorbox: function($dom){
+    $dom.colorbox({
+      transition: "none",
+      rel: "img",
+      maxWidth: "100%",
+      maxHeight: "100%",
+      initialWidth: "200px",
+      initialHeight: "200px"
+    });
+  },
+
+  init_notification: function(){
+    if(window.localStorage){
+      if(window.localStorage.popupNotification == 'true'){
+        $('#notification').attr('checked', 'checked');
+      }
+
+      $('#notification').on('click', function(){
+        if($(this).attr('checked') == 'checked'){
+          window.localStorage.popupNotification = 'true';
+        }else{
+          window.localStorage.popupNotification = 'false';
+        }
+      });
+
+      if (window.localStorage.notificationSeconds){
+        $('#notification_seconds').val(window.localStorage.notificationSeconds);
+      }else{
+        $('#notification_seconds').val(5);
+        window.localStorage.notificationSeconds = 5;
+      } 
+      $('#notification_seconds').on('change',function(){
+        window.localStorage.notificationSeconds = $(this).val();
+      });
+    }else{
+      $('#notification').attr('disabled', 'disabled');
+    }
+  },
+
+  do_notification: function(data){
+    var notif_title = data.name + (TITLE_NAME != "" ? " @" + TITLE_NAME : "");
+    var notif_icon = 'notification.png';
+    var notif_msg = data.msg;
+
+    if (window.localStorage.popupNotification == 'true'){
+      if (window.webkitNotifications){
+        var havePermission = window.webkitNotifications.checkPermission();
+        if (havePermission == 0) {
+          var notification = window.webkitNotifications.createNotification(
+              notif_icon,
+              notif_title,
+              notif_msg
+              );
+          notification.onclick = function () {
+            notification.close();
+          }
+          notification.show();
+          setTimeout(function(){
+            notification.cancel();
+          }, window.localStorage.notificationSeconds * 1000);
+        } else {
+          window.webkitNotifications.requestPermission();
+        }
+      }else if(Notification){
+        Notification.requestPermission(function(permission){
+          if(Notification.permission == "granted"){
+            switch(Notification.permission){
+              case "granted":
+              case "default":
+                new Notification(notif_title, {
+                  icon:notif_icon,
+                    body:notif_msg,
+                    tag:"notification-test",
+                });
+                break;
+              case "denied":
+                break;
+            }
+          }
+        });
+      }
+    }
+  }
+}
--- static/javascripts/libs/client.js
+++ static/javascripts/libs/client.js
@@ -1,6 +1,4 @@
-var latest_login_list = [];
 var login_name = '';
-var suggest_obj = undefined;
 var LOGIN_COLOR_MAX = 9;
 var COOKIE_NAME = "dev_hub_name";
 var COOKIE_STYLE_NAME = "dev_hub_style_name";
@@ -33,6 +31,9 @@ var is_mobile = false;
 var writing_text = [];
 var text_logs = [];
 
+// Controllers
+var chatController = null;
+
 // for favicon
 var faviconNumber = null;
 
@@ -41,15 +42,18 @@ $(function() {
     is_mobile = true;
   }
 
-  init_chat();
-  init_sharememo();
-  init_websocket();
-  init_dropzone();
-  init_notification();
-
   faviconNumber = new FaviconNumber({
     focus_id: "message"
   });
+
+  chatController = new ChatController({
+    socket: socket,
+    faviconNumber: faviconNumber
+  });
+
+  init_sharememo();
+  init_websocket();
+  init_dropzone();
 
   // for smartphone
   // 本当は bootstrap-responsive のみやりたいが、perfectScrollbar の制御は
@@ -117,21 +121,6 @@ function adjust_display_size_for_mobile(){
     $('#chat_area').css('width',window_width + 'px').css('margin',0);
     $('#memo_area').css('width',window_width + 'px').css('margin',0);
     Flipsnap('.flipsnap').refresh();
-}
-
-function init_chat(){
-  $('#list').on('click', '.remove_msg', function(){
-    var id = "#" + $(this).closest('li').attr('id');
-    var data_id = $(this).closest('li').data('id');
-    $(id).fadeOut();
-    send_remove_msg(data_id);
-  });
-
-  $('#list').on('click','.login-name-base', function(){
-    var name = $(this).text();
-    $('#message').val($('#message').val() + " @" + name + "さん ");
-    $('#message').focus();
-  });
 }
 
 function init_sharememo(){
@@ -211,79 +200,9 @@ function init_websocket(){
   });
 
   // for chat
-  socket.on('message_own', function(data) {
-    var $msg = prepend_own_msg(data);
-    setColorbox($msg.find('.thumbnail'));
-    faviconNumber.up();
-  });
+  chatController.bindSocketEvent();
 
-  socket.on('message', function(data) {
-    var $msg = prepend_msg(data);
-    setColorbox($msg.find('.thumbnail'));
-    do_notification(data);
-    faviconNumber.up();
-  });
-
-  socket.on('remove_message', function(data) {
-    $('#msg_' + data.id).fadeOut();
-  });
-
-  socket.on('list', function(login_list) {
-    $('#login_list_loader').hide();
-
-    var out_list = "";
-    for (var i = 0; i < login_list.length; ++i){
-      var login_elem = "";
-      var place = "";
-      if ( login_list[i].place != "" ){
-        place = "@" + login_list[i].place;
-      }
-      if ( login_list[i].pomo_min > 0 ){
-        login_elem = '<span class="login-elem login-name-pomo"><span class="name">' + login_list[i].name + '</span>' + place + ' <span class="pomo-min">' + login_list[i].pomo_min + 'min</span></span>'
-      }else{
-        login_elem = '<span class="login-elem login-name' + get_color_id_by_name_id(login_list[i].id) + '"><span class="name">' + login_list[i].name + '</span>' + place + '</span>'
-      }
-      out_list += login_elem + "<wbr>";
-    }
-    out_list = "<nobr>" + out_list + "</nobr>";
-
-    if ($('#login_list').html() != out_list){
-      $('#login_list').html(out_list);
-      $('#login_list').fadeIn();
-      suggest_start(login_list);
-
-      // add click event for each login names.
-      $('#login_list .login-elem').click(function(){
-        var name = $(this).children(".name").text();
-        $('#message').val($('#message').val() + " @" + name + "さん ");
-        $('#message').focus();
-      });
-    }
-
-    latest_login_list = login_list.sort(function(a,b){ return b.name.length - a.name.length });
-  });
-
-  socket.on('latest_log', function(msgs) {
-    for ( var i = 0 ; i < msgs.length; i++){
-      append_msg(msgs[i])
-    }
-
-    setColorbox($('#chat_body').find('.thumbnail'));
-  });
-
-  $('#form').submit(function() {
-    var name = $('#name').val();
-    var message = $('#message').val();
-    $.cookie(COOKIE_NAME,name,{ expires: COOKIE_EXPIRES });
-
-    if ( message && name ){
-      login_name = name;
-      socket.emit('message', {name:name, msg:message});
-      $('#message').attr('value', '');
-    }
-    return false;
-  });
-
+  // for share memo
   $(".code").autofit({min_height: CODE_MIN_HEIGHT});
 
   function setCaretPos(item, pos) {
@@ -658,166 +577,6 @@ function init_websocket(){
   });
 };
 
-function suggest_start(list){
-  var suggest_list = []
-  for (var i = 0; i < list.length; ++i){
-    suggest_list.push("@" + list[i].name + "さん");
-  }
-
-  if (suggest_obj == undefined){
-    suggest_obj = new Suggest.LocalMulti("message", "suggest", suggest_list, {interval: 200, dispAllKey: false, prefix: true, highlight: true});
-  }else{
-    suggest_obj.candidateList = suggest_list;
-  }
-}
-
-function append_msg(data){
-  //TODO: System メッセージを非表示にする。
-  //      切り替え可能にするかは検討する。
-  if (data.name == "System") { return };
-  if (exist_msg(data)){ return };
-
-  var msg = get_msg_html(data);
-
-  $('#list').append(msg.li.addClass(msg.css));
-  msg.li.fadeIn();
-};
-
-function prepend_own_msg(data){
-  if (exist_msg(data)){ return };
-  var msg = get_msg_html(data);
-
-  $('#list').prepend(msg.li);
-  msg.li.addClass("text-highlight",0);
-  msg.li.slideDown('fast',function(){
-    msg.li.switchClass("text-highlight", msg.css, 500);
-  });
-
-  return msg.li;
-};
-
-function send_remove_msg(id){
-  socket.emit('remove_message', {id:id});
-}
-
-function prepend_msg(data){
-  //TODO: System メッセージを非表示にする。
-  //      切り替え可能にするかは検討する。
-  if (data.name == "System") { return }
-  if (exist_msg(data)){ return };
-
-  var msg = get_msg_html(data);
-
-  $('#list').prepend(msg.li);
-  msg.li.addClass("text-highlight",0);
-  msg.li.slideDown('fast',function(){
-    msg.li.switchClass("text-highlight", msg.css, 500);
-  });
-
-  return msg.li;
-};
-
-function exist_msg(data){
-  if (data.msg == undefined) { data.msg = ""; }
-  var id = '#msg_' + data._id.toString();
-  return $(id).size() > 0;
-}
-
-function get_msg_html(data){
-  if ( data.name == login_name ){
-    return {
-      li: get_msg_li_html(data).html(get_msg_body(data) + '<a class="remove_msg">x</a><span class="own_msg_date">' + data.date + '</span></td></tr></table>'),
-      css: "own_msg"
-    };
-  } else if (include_target_name(data.msg,login_name)){
-    return {
-      li: get_msg_li_html(data).html(get_msg_body(data) + ' <span class="target_msg_date">' + data.date + '</span></td></tr></table>'),
-      css: "target_msg"
-    };
-  }else{
-    return {
-      li: get_msg_li_html(data).html(get_msg_body(data) + ' <span class="date">' + data.date + '</span></td></tr></table>'),
-      css: null
-    };
-  }
-}
-
-function get_msg_li_html(data){
-  if ( data._id != undefined ){
-    return $('<li/>').attr('style','display:none').attr('id','msg_' + data._id.toString()).attr('data-id', data._id.toString());
-  }else{
-    return $('<li/>').attr('style','display:none');
-  }
-}
-
-function get_msg_body(data){
-  var date = new Date();
-  var id = date.getTime();
-
-  var name_class = "login-name";
-  var msg_class = "msg";
-
-  data.id = get_id(data.name)
-
-  if ( data.name == "System" ){
-    name_class = "login-name-system";
-    msg_class = "msg_ext"
-  }else if ( data.ext == true ){
-    name_class = "login-name-ext";
-    msg_class = "msg_ext"
-  }else if ( data.name == "Pomo" ){
-    name_class = "login-name-pomosys";
-    msg_class = "msg_pomo"
-  }else{
-    name_class = "login-name" + get_color_id_by_name_id(data.id);
-  }
-
-  return '<table><tr><td nowrap valign="top"><span class="login-name-base ' + name_class + '">' + data.name + '</span></td><td width="100%"><span class="msg_text ' + msg_class + '">' + decorate_msg(data.msg) + '</span>';
-}
-
-function get_color_id_by_name_id(id){
-  if(id == 0){ return 0; } // no exist user.
-  return id % LOGIN_COLOR_MAX + 1; // return 1 〜 LOGIN_COLOR_MAX
-}
-
-function decorate_msg(msg){
-  var deco_msg = msg;
-
-  deco_msg = deco_login_name(deco_msg)
-  deco_msg = $.decora.message_to_html(deco_msg);
-
-  return deco_msg;
-};
-
-function deco_login_name(msg){
-  var deco_msg = msg;
-  var name_reg = RegExp("@(.+?)さん", "g");
-  deco_msg = deco_msg.replace( name_reg, function(){
-    if (arguments[1] == login_name){
-      return '<span class="label label-important">' + arguments[0] + '</span>'
-    }else{
-      return '<span class="label label-info">' + arguments[0] + '</span>'
-    }
-  });
-  return deco_msg;
-}
-
-function include_target_name(msg,name){
-  var name_reg = RegExp("@" + name + "( |　|さん|$)");
-  if (msg.match(name_reg)){
-    return true;
-  }
-  return false;
-}
-
-function get_id(name){
-  for(var i = 0; i < latest_login_list.length; ++i ){
-    if ( latest_login_list[i].name == name ){
-      return latest_login_list[i].id;
-    }
-  }
-  return 0;
-}
 
 function change_style(style_name) {
   if (!(style_name in STYLE_CONFIG)) {
@@ -956,79 +715,6 @@ function init_dropzone(){
   $('#upload_chat').on('change', select_file_action($('#alert_chat_area'), function(that, res){
     $('#message').val($('#message').val() + ' ' + res.fileName + ' ');
   }));
-}
-
-function init_notification(){
-  if(window.localStorage){
-    if(window.localStorage.popupNotification == 'true'){
-      $('#notification').attr('checked', 'checked');
-    }
-
-    $('#notification').on('click', function(){
-      if($(this).attr('checked') == 'checked'){
-        window.localStorage.popupNotification = 'true';
-      }else{
-        window.localStorage.popupNotification = 'false';
-      }
-    });
-
-    if (window.localStorage.notificationSeconds){
-      $('#notification_seconds').val(window.localStorage.notificationSeconds);
-    }else{
-      $('#notification_seconds').val(5);
-      window.localStorage.notificationSeconds = 5;
-    } 
-    $('#notification_seconds').on('change',function(){
-      window.localStorage.notificationSeconds = $(this).val();
-    });
-  }else{
-    $('#notification').attr('disabled', 'disabled');
-  }
-}
-
-function do_notification(data){
-  var notif_title = data.name + (TITLE_NAME != "" ? " @" + TITLE_NAME : "");
-  var notif_icon = 'notification.png';
-  var notif_msg = data.msg;
-
-  if(window.localStorage.popupNotification == 'true'){
-    if (window.webkitNotifications){
-      var havePermission = window.webkitNotifications.checkPermission();
-      if (havePermission == 0) {
-        var notification = window.webkitNotifications.createNotification(
-            notif_icon,
-            notif_title,
-            notif_msg
-            );
-        notification.onclick = function () {
-          notification.close();
-        }
-        notification.show();
-        setTimeout(function(){
-          notification.cancel();
-        }, window.localStorage.notificationSeconds * 1000);
-      } else {
-        window.webkitNotifications.requestPermission();
-      }
-    }else if(Notification){
-      Notification.requestPermission(function(permission){
-        if(Notification.permission == "granted"){
-          switch(Notification.permission){
-            case "granted":
-            case "default":
-              new Notification(notif_title, {
-                icon:notif_icon,
-                  body:notif_msg,
-                  tag:"notification-test",
-              });
-              break;
-            case "denied":
-              break;
-          }
-        }
-      });
-    }
-  }
 }
 
 function setColorbox($dom){
