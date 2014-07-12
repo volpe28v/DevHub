31d9853844a8120ac2b11cf3c41f8039e2de3b60
naoki kodama <naoki.koda@gmail.com>
2014-07-13 00:42:28 +0900

    add index list and blog header.

--- static/javascripts/libs/blog.js
+++ static/javascripts/libs/blog.js
@@ -1,18 +1,34 @@
+var COOKIE_NAME = "dev_hub_name";
+
 $(function() {
+  var name = $.cookie(COOKIE_NAME);
   emojify.setConfig({
     img_dir: 'img/emoji',  // Directory for emoji images
   });
 
   function add_blog(blog){
-    var $target = $('<div/>')
+    var id = blog._id;
+    var title = blog.text.split("\n")[0];
+
+    // ブログの追加
+    var $target = $('<div/>').attr("id", id)
+          .append($('<div/>').addClass('blog-header')
+            .html('<strong>' + blog.name + '</strong> added <span data-livestamp="' + blog.date + '"></span>')
+            .append($('<div/>').css('float','right').html('<span class="edit-blog"><i class="icon-pencil"></i></span> <span class="remove-blog"><i class="icon-remove"></i></span>')))
           .append($('<pre/>').addClass('text-base-style')
             .append($('<div/>').addClass('code-out'))
           );
 
     $target.find('.code-out').showDecora(blog.text);
     $('#blog-list').prepend($target);
+
+    // 見出しの追加
+    var $index = $('<li/>')
+          .append($('<a/>').attr('href',"#").attr('data-id',id).html(title + ' - <span data-livestamp="' + blog.date + '"></span>'));
+    $('#index_list').prepend($index);
   }
  
+  // 初期リスト読み込み
   $.ajax('blog/body' , {
     type: 'GET',
     cache: false,
@@ -23,6 +39,7 @@ $(function() {
     }
   });
 
+  // 保存イベント
   $('#save_btn').click(function(){
     var blog_text = $('#blog_form').val();
     if (blog_text == ""){ return; }
@@ -31,11 +48,16 @@ $(function() {
     $.ajax('blog' , {
       type: 'POST',
       cache: false,
-      data: {blog: {text: blog_text}},
+      data: {blog: {text: blog_text, name: name}},
       success: function(data){
         add_blog(data.blog);
-        console.log(data.blog);
       }
     });
+  });
+
+  // インデクスクリックイベント
+  $('#index_list').on("click","a", function(){
+    $target = $("#" + $(this).data('id'));
+    $('html,body').animate({ scrollTop: $target.offset().top - 60}, 'fast');
   });
 });
