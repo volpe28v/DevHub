0ef0e8ab6e7bf56a965b8e518b4465aa46762754
naoki kodama <naoki.koda@gmail.com>
2014-07-15 00:45:49 +0900

    refactoring blog.js.

--- static/javascripts/libs/blog.js
+++ static/javascripts/libs/blog.js
@@ -7,32 +7,7 @@ $(function() {
   });
 
   // blog のモデルとViewをバインド
-  var Blogs = [];
-  $.templates("#blogBodyTmpl").link("#blog_list", Blogs);
-  $.templates("#blogIndexTmpl").link("#index_list", Blogs);
-
-  function add_blog(blog){
-    var id = blog._id;
-    blog.title = blog.text.split("\n")[0];
-
-    $.observable(Blogs).insert(0,blog);
-    var $target = $('#' + id);
-    $target.find(".code-out").showDecora(blog.text);
-  }
- 
-  // 初期リスト読み込み
-  $.ajax('blog/body' , {
-    type: 'GET',
-    cache: false,
-    success: function(data){
-      var blogs = data.body;
-      blogs.forEach(function(blog){
-        add_blog(blog);
-      });
-
-      console.log(blogs);
-    }
-  });
+  var blogViewModel = new BlogViewModel();
 
   // 保存イベント
   $('#save_btn').click(function(){
@@ -40,86 +15,33 @@ $(function() {
     if (blog_text == ""){ return; }
     $('#blog_form').val("");
 
-    $.ajax('blog' , {
-      type: 'POST',
-      cache: false,
-      data: {blog: {text: blog_text, name: name}},
-      success: function(data){
-        add_blog(data.blog);
+    var blog = {text: blog_text, name: name};
+    blogViewModel.add(blog);
+  });
+
+  // ViewとViewModelをバインド
+  $.templates("#blogBodyTmpl").link("#blog_list", blogViewModel.items)
+    .on("click",".edit-blog", function(){
+      blogViewModel.edit($.view(this));
+    })
+    .on("click",".update-blog", function(){
+      blogViewModel.update($.view(this));
+    })
+    .on("click",".cancel-edit", function(){
+      blogViewModel.cancel($.view(this));
+    })
+    .on("click",".remove-blog", function(){
+      if (window.confirm('本当に削除しますか？')){
+        blogViewModel.destory($.view(this));
       }
     });
-  });
 
-  // ブログを編集
-  $('#blog_list').on("click",".edit-blog", function(){
-    var index = $.view(this).index;
-    var blog = Blogs[index];
-    blog.pre_text = blog.text;
-
-    var $target = $(this).closest('.blog-body');
-    $target.find('pre').hide();
-    $target.find('.edit-form').show();
-    $target.find('textarea').focus().autofit({min_height: 100});
- });
-
-  // ブログを更新
-  $('#blog_list').on("click",".update-blog", function(){
-    var index = $.view(this).index;
-    var blog = Blogs[index];
-
-    // 名前・タイトルを更新
-    var title = blog.text.split("\n")[0];
-    $.observable(blog).setProperty("title", title);
-    $.observable(blog).setProperty("name", name);
-
-    var $target = $(this).closest('.blog-body');
-    $target.find(".code-out").showDecora(blog.text);
-    $target.find('pre').show();
-    $target.find('.edit-form').hide();
-
-    $.ajax('blog' , {
-      type: 'POST',
-      cache: false,
-      data: {blog: {_id: blog._id, text: blog.text, name: name}},
-      success: function(data){
-        $.observable(blog).setProperty("date", data.blog.date);
-      }
+  $.templates("#blogIndexTmpl").link("#index_list", blogViewModel.items)
+    .on("click","a", function(){
+      $target = $("#" + $(this).data('id'));
+      $('html,body').animate({ scrollTop: $target.offset().top - 60}, 'fast');
     });
-  });
 
-  // ブログ編集をキャンセル
-  $('#blog_list').on("click",".cancel-edit", function(){
-    var index = $.view(this).index;
-    var blog = Blogs[index];
-    $.observable(blog).setProperty("text", blog.pre_text);
-
-    var $target = $(this).closest('.blog-body');
-    $target.find('pre').show();
-    $target.find('.edit-form').hide();
-  });
- 
-  // ブログを削除
-  $('#blog_list').on("click",".remove-blog", function(){
-    if (window.confirm('本当に削除しますか？')){
-      var index = $.view(this).index;
-      var remove_blog = {};
-      remove_blog._id = Blogs[index]._id;
-
-      $.ajax('blog' , {
-        type: 'DELETE',
-        cache: false,
-        data: {blog: remove_blog},
-        success: function(data){
-        }
-      });
-
-      $.observable(Blogs).remove(index);
-    }
-  });
-
-  // インデクスクリックイベント
-  $('#index_list').on("click","a", function(){
-    $target = $("#" + $(this).data('id'));
-    $('html,body').animate({ scrollTop: $target.offset().top - 60}, 'fast');
-  });
+  // 初期リスト読み込み
+  blogViewModel.refresh();
 });
---
+++ static/javascripts/libs/blog_view_model.js
@@ -0,0 +1,102 @@
+function BlogViewModel(){
+  this.items = [];
+
+}
+
+BlogViewModel.prototype = {
+  refresh: function(){
+    var that = this;
+    $.ajax('blog/body' , {
+      type: 'GET',
+      cache: false,
+      success: function(data){
+        var blogs = data.body;
+        blogs.forEach(function(blog){
+          that._addItem(blog);
+        });
+      }
+    });
+  },
+
+  add: function(item){
+    var that = this;
+    $.ajax('blog' , {
+      type: 'POST',
+      cache: false,
+      data: {blog: item},
+      success: function(data){
+        that._addItem(data.blog);
+      }
+    });
+  },
+
+  edit: function(view){
+    var index = view.index;
+    var blog = this.items[index];
+    blog.pre_text = blog.text;
+
+    var $target = $(view.contents()).closest('.blog-body');
+    $target.find('pre').hide();
+    $target.find('.edit-form').show();
+    $target.find('textarea').focus().autofit({min_height: 100});
+  },
+
+  update: function(view){
+    var index = view.index;
+    var blog = this.items[index];
+
+    // 名前・タイトルを更新
+    var title = blog.text.split("\n")[0];
+    $.observable(blog).setProperty("title", title);
+    $.observable(blog).setProperty("name", name);
+
+    var $target = $(view.contents()).closest('.blog-body');
+    $target.find(".code-out").showDecora(blog.text);
+    $target.find('pre').show();
+    $target.find('.edit-form').hide();
+
+    $.ajax('blog' , {
+      type: 'POST',
+      cache: false,
+      data: {blog: {_id: blog._id, text: blog.text, name: name}},
+      success: function(data){
+        $.observable(blog).setProperty("date", data.blog.date);
+      }
+    });
+  },
+ 
+  cancel: function(view){
+    var index = view.index;
+    var blog = this.items[index];
+    $.observable(blog).setProperty("text", blog.pre_text);
+
+    var $target = $(view.contents()).closest('.blog-body');
+    $target.find('pre').show();
+    $target.find('.edit-form').hide();
+  },
+
+  destory: function(view){
+    var index = view.index;
+    var remove_blog = {};
+    remove_blog._id = this.items[index]._id;
+
+    $.ajax('blog' , {
+      type: 'DELETE',
+      cache: false,
+      data: {blog: remove_blog},
+      success: function(data){
+      }
+    });
+
+    $.observable(this.items).remove(index);
+  },
+
+  _addItem: function(item){
+    var id = item._id;
+    item.title = item.text.split("\n")[0];
+
+    $.observable(this.items).insert(0,item);
+    var $target = $('#' + id);
+    $target.find(".code-out").showDecora(item.text);
+  }
+}
