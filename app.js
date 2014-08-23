975392dae4a67954112e8b07780e25bcd5724b58
naoki kodama <naoki.koda@gmail.com>
2014-08-23 23:09:18 +0900

    add avatar argument to API of notify. #289

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ app.get('/notify', function(req, res) {
   console.log('/notify');
   var name = unescape(req.query.name);
   var msg = unescape(req.query.msg);
-  var data = {name: name, msg: msg, date: util.getFullDate(new Date()), ext: true};
+  var avatar = unescape(req.query.avatar);
+  var data = {name: name, msg: msg, avatar: avatar, date: util.getFullDate(new Date()), ext: true};
 
   // 内容が無いものはスルー
   if (name == "" || msg == ""){ return; }
