c650f6c7a75e5aa72cb354e8f2bdb3a5f0e72045
naoki kodama <naoki.koda@gmail.com>
2014-09-07 17:07:06 +0900

    add test case of share_memo.

--- nightwatch/tests/share_memo.js
+++ nightwatch/tests/share_memo.js
@@ -87,7 +87,71 @@ module.exports = {
       .assert.attributeEquals('#share_memo_1 .code-out font', 'color', '#387d39')
       .assert.containsText('#share_memo_1 .code-out font', 'this is green color line.')
 
+      .click('#share_memo_1 .sync-text')
+      .waitForElementVisible('#share_memo_1 .code', 1000)
+      .pause(500)
+      .clearValue('#share_memo_1 .code')
+      .setValue('#share_memo_1 .code', 'this is other color line. #112233\n')
+      .click('#share_memo_1 .fix-text')
+      .waitForElementVisible('#share_memo_1 .code-out', 1000)
+      .assert.attributeEquals('#share_memo_1 .code-out font', 'color', '#112233')
+      .assert.containsText('#share_memo_1 .code-out font', 'this is other color line.')
+  },
 
+  'チェックボックスを入力' : function (client) {
+    client
+      .click('#share_memo_1 .sync-text')
+      .waitForElementVisible('#share_memo_1 .code', 1000)
+      .pause(500)
+      .clearValue('#share_memo_1 .code')
+      .setValue('#share_memo_1 .code', '-[] this is a task.\n')
+      .click('#share_memo_1 .fix-text')
+      .waitForElementVisible('#share_memo_1 .code-out', 1000)
+      .assert.attributeEquals('#share_memo_1 .code-out input', 'type', 'checkbox')
+      .assert.containsText('#share_memo_1 .code-out', 'this is a task.')
+  },
+
+  'コードを入力' : function (client) {
+    client
+      .click('#share_memo_1 .sync-text')
+      .waitForElementVisible('#share_memo_1 .code', 1000)
+      .pause(500)
+      .clearValue('#share_memo_1 .code')
+      .setValue('#share_memo_1 .code', '```\n')
+      .setValue('#share_memo_1 .code', 'this is a code.\n')
+      .setValue('#share_memo_1 .code', '```\n')
+      .click('#share_memo_1 .fix-text')
+      .waitForElementVisible('#share_memo_1 .code-out', 1000)
+      .assert.containsText('#share_memo_1 .code-out .code-out-pre', 'this is a code.')
+  },
+
+  'URLを入力' : function (client) {
+    client
+      .click('#share_memo_1 .sync-text')
+      .waitForElementVisible('#share_memo_1 .code', 1000)
+      .pause(500)
+      .clearValue('#share_memo_1 .code')
+      .setValue('#share_memo_1 .code', 'https://github.com/volpe28v/DevHub\n')
+      .click('#share_memo_1 .fix-text')
+      .waitForElementVisible('#share_memo_1 .code-out', 1000)
+      .assert.containsText('#share_memo_1 .code-out a', 'https://github.com/volpe28v/DevHub')
+  },
+
+  '画像を入力' : function (client) {
+    client
+      .click('#share_memo_1 .sync-text')
+      .waitForElementVisible('#share_memo_1 .code', 1000)
+      .pause(500)
+      .clearValue('#share_memo_1 .code')
+      .setValue('#share_memo_1 .code', 'http://nightwatchjs.org/img/logo-nightwatch.png\n')
+      .click('#share_memo_1 .fix-text')
+      .waitForElementVisible('#share_memo_1 .code-out', 1000)
+      .assert.attributeEquals('#share_memo_1 img', 'src', 'http://nightwatchjs.org/img/logo-nightwatch.png')
+  },
+
+
+  '共有メモテスト終了' : function (client) {
+    client
       .end();
   }
 };
