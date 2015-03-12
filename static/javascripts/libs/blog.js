d3c06a7a1b9f4db3c7f788ed1c598d8067c72662
naoki kodama <naoki.koda@gmail.com>
2015-03-13 00:57:59 +0900

    ブログのレイアウト変更。インデクスを左にして一列に並べた

--- static/javascripts/libs/blog.js
+++ static/javascripts/libs/blog.js
@@ -142,7 +142,7 @@ $(function() {
       $target = $("#" + $(this).data('id'));
       var target_top = $target.offset().top;
       var base_top = $("#blog_list").offset().top;
-      $('#blog_area').scrollTop(target_top - base_top + 54);
+      $('#blog_area').scrollTop(target_top - base_top + 48);
     })
     .on('inview', '.index-body:last-child', function(event, isInView, visiblePartX, visiblePartY) {
       blogViewModel.load_more();
