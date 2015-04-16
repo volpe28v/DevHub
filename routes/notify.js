9decf54a1888eb55940d3514a58eabb57d5c0b51
naoki kodama <naoki.koda@gmail.com>
2015-04-16 22:55:09 +0900

    汎用APIに room_id を追加

--- routes/notify.js
+++ routes/notify.js
@@ -8,13 +8,14 @@ exports.get = function(req, res, io) {
   var name = unescape(req.query.name);
   var msg = unescape(req.query.msg);
   var avatar = req.query.avatar != undefined ? unescape(req.query.avatar) : null;
-  var data = {name: name, msg: msg, avatar: avatar, date: util.getFullDate(new Date()), ext: true};
+  var room_id = req.query.room_id != undefined ? req.query.room_id : 1;
+  var data = {name: name, msg: msg, avatar: avatar, room_id: room_id, date: util.getFullDate(new Date()), ext: true};
 
   // 内容が無いものはスルー
   if (name == "" || msg == ""){ return; }
 
   chat_log.add(data,function(){
-    io.sockets.emit('message', data);
+    io.sockets.emit('message' + room_id, data);
     client_info.send_growl_all(data);
     res.end('received msg');
   });
@@ -24,7 +25,7 @@ exports.get = function(req, res, io) {
     setTimeout(function(){
       reply.date = util.getFullDate(new Date());
       chat_log.add(reply);
-      io.sockets.emit('message', reply);
+      io.sockets.emit('message' + room_id, reply);
       client_info.send_growl_all(reply);
     },reply.interval * 1000);
   });
