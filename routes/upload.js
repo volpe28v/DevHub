9da64e86c8dafdece55459c8b365452c8263d8eb
naoki kodama <naoki.koda@gmail.com>
2016-03-17 00:33:02 +0900

    ファイルアップロードに対応。ファイル直置き版

--- routes/upload.js
+++ routes/upload.js
@@ -1,30 +1,45 @@
+var multiparty = require('multiparty');
 var fs = require('fs');
 var util = require('../lib/util');
+var GridFs = require('grid-fs');
 
 module.exports.set_db = function(current_db){
 
 };
 
-exports.post = function(req, res) {
-  var tmp_path = req.files.file.path;
-  var base_name = req.files.file.name.replace(/[ 　]/g,'_');
+function modifyFileName(file_name, type){
+  var base_name = file_name.replace(/[ 　]/g,'_');
   // 画像貼り付けの場合は拡張子を付加する
-  if (base_name == "blob" && req.files.file.type.indexOf('image') != -1){
-    base_name = "paste_image." + req.files.file.type.split('/')[1];
+  if (base_name == "blob" && type.indexOf('image') != -1){
+    base_name = "paste_image." + type.split('/')[1];
   }
-  var file_name = util.getFullDateNoSepa(new Date()) + '_' + base_name; // 半角・全角スペースを置き換え
-  var target_path = './static/uploads/' + file_name;
-  var access_path = '/uploads/' + file_name;
+  return util.getFullDateNoSepa(new Date()) + '_' + base_name; // 半角・全角スペースを置き換え
+}
 
-  fs.rename(tmp_path, target_path, function(err) {
-    if (err) {
-      throw err;
-    }
-    fs.unlink(tmp_path, function() {
-      if (err) {
-        throw err;
-      }
-      res.send({fileName: access_path});
+exports.post = function(req, res) {
+  var form = new multiparty.Form();
+
+  form.parse(req, function(err, fields, files) {
+    Object.keys(files).forEach(function(name) {
+      var org_name = files[name][0].originalFilename;
+      var type = files[name][0].headers['content-type'];;
+      var file_name = modifyFileName(org_name, type);
+      var tmp_path = files[name][0].path;
+      var target_path = './static/uploads/' + file_name;
+      var access_path = '/uploads/' + file_name;
+
+      fs.rename(tmp_path, target_path, function(err) {
+        if (err) {
+          throw err;
+        }
+        fs.unlink(tmp_path, function() {
+          if (err) {
+            throw err;
+          }
+          res.send({fileName: access_path});
+        });
+      });
+      return;
     });
   });
 };
