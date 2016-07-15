f44d558311c52318615197835325fd68862d1885
naoki kodama <naoki.koda@gmail.com>
2016-07-15 22:54:23 +0900

    windowsのファイルパスの区切りを修正。

--- static/javascripts/libs/jquery.decora.js
+++ static/javascripts/libs/jquery.decora.js
@@ -472,11 +472,11 @@ var prettify = require('prettify');
   }
 
   function _decorate_windows_path( text ){
-    var win_path = text.replace(/(^|[ ]+)((¥¥[^\s　]+|[a-zA-Z]:)¥[^\s　]+)/mg,
+    var win_path = text.replace(/(^|[ ]+)((\\\\[^\s　]+|[a-zA-Z]:)\\[^\s　]+)/mg,
         function(){
           var prefix = arguments[1];
           var matched = arguments[2];
-          return prefix + '<span class="win-path" data-clipboard-text="' + matched + '" data-bind="clippable: true, tooltip: \'bottom\'" title="Copy to clipboard">' + matched + '</span>';
+          return prefix + '<span class="win-path" data-clipboard-text="' + matched + '" data-bind="clippable: true, tooltip: \'bottom\'" title="Copy">' + matched + '</span>';
         });
     return win_path;
   }
