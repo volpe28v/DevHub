60c45a64f11a09de7a8b0debe67cd9decdbb767d
naoki kodama <naoki.koda@gmail.com>
2016-08-07 20:49:49 +0900

    設定関係のプロパティを SettingViewModel に移譲した

--- static/javascripts/app/client.js
+++ static/javascripts/app/client.js
@@ -1,6 +1,3 @@
-var COOKIE_NAME = "dev_hub_name";
-var COOKIE_EXPIRES = 365;
-
 global.jQuery = require('jquery');
 global.$ = global.jQuery;
 global.moment = require('moment');
@@ -11,7 +8,6 @@ require('bootstrap');
 require('jquery-colorbox');
 require('jquery.cookie');
 require('../libs/livestamp');
-require('ion-sound');
 
 var Flipsnap = require('flipsnap');
 var emojify = require('emojify.js');
@@ -26,6 +22,7 @@ require('../libs/jquery.autosize');
 var FaviconNumber = require('../libs/favicon-number');
 var DropZone = require('../libs/dropzone');
 
+var SettingViewModel = require('./setting_view_model');
 var MemoController = require('./memo_controller');
 var ChatController = require('./chat_controller');
 
@@ -34,114 +31,12 @@ function ClientViewModel(){
 
   this.socket = socket;
   this.is_mobile = false;
-  this.loginName = ko.observable($.cookie(COOKIE_NAME));
-  this.loginName.subscribe(function(value){
-    that.chatController.setName(value);
-    that.memoController.setName(value);
-  });
-  this.avatar = ko.observable(window.localStorage.avatarImage != null ? window.localStorage.avatarImage : "");
-
-  // notification mode
-  this.notificationMode = ko.observable(window.localStorage.popupNotification != null ? window.localStorage.popupNotification : 'disable');
-  this.notificationMode.subscribe(function(newValue){
-    window.localStorage.popupNotification = newValue;
-    if (newValue != "disable"){
-      if(Notification){
-        Notification.requestPermission();
-      }
-    }
-  });
-  this.notificationSeconds = ko.observable(window.localStorage.notificationSeconds != null ? window.localStorage.notificationSeconds : 5);
-  this.notificationSeconds.subscribe(function(newValue){
-    window.localStorage.notificationSeconds = newValue;
-  });
-
-  // notification sound
-  this.notificationSoundMode = ko.observable(window.localStorage.notificationSoundMode == "on" ? "on" : "off");
-  this.notificationSoundMode.subscribe(function(newValue){
-    window.localStorage.notificationSoundMode = newValue;
-  });
-
-  this.notificationSounds = ko.observableArray([
-    { name: "snap" ,           dispName: "snap" ,    alias: "s2" },
-    { name: "pop_cork",        dispName: "pop_cork", alias: "s9" },
-    { name: "tap",             dispName: "tap",      alias: "s10"},
-    { name: "glass",           dispName: "glass",    alias: "s1" },
-    { name: "water_droplet",   dispName: "water1",   alias: "s3" },
-    { name: "water_droplet_2", dispName: "water2",   alias: "s4" },
-    { name: "water_droplet_3", dispName: "water3",   alias: "s5" },
-    { name: "button_click_on", dispName: "button1",  alias: "s6" },
-    { name: "button_push"    , dispName: "button2",  alias: "s7" },
-    { name: "button_tiny",     dispName: "button3",  alias: "s8" },
-    { name: "upload",          dispName: "*upload sound*",  alias: "up" },
-  ]);
-  var notiSound = this.notificationSounds().filter(function(sound){ return sound.alias == window.localStorage.notificationSound; })[0];
-  this.selectedNotiSound = ko.observable(notiSound != null ? notiSound.alias : this.notificationSounds()[0].alias);
-  this.selectedNotiSound.subscribe(function(newValue){
-    window.localStorage.notificationSound = newValue;
-  });
-  this.uploadedSound = ko.observable(window.localStorage.notificationUploadedSound);
-  this.uploadedSound.subscribe(function(newValue){
-    window.localStorage.notificationUploadedSound = newValue;
-  });
-
-  this.doNotification = function(data, isMention, room){
-    if (that.notificationMode() == 'true' ||
-        (that.notificationMode() == 'mention' && isMention)){
-      if(Notification){
-        if (Notification.permission != "denied"){
-          that._do_notification(data, room);
-          that.playNotificationSound();
-        }
-      }else{
-        Notification.requestPermission();
-      }
-    }
-  }
-
-  this._do_notification = function(data, room){
-    var notif_title = data.name + (TITLE_NAME != "" ? " @" + TITLE_NAME : "") + " -> " + room;
-    var notif_icon = 'notification.png';
-    if (data.avatar != null && data.avatar != "" && data.avatar != "undefined"){
-      notif_icon = data.avatar;
-    }
-    var notif_msg = data.msg;
-
-    var notification = new Notification(notif_title, {
-      icon: notif_icon,
-      body: notif_msg
-    });
-    setTimeout(function(){
-      notification.close();
-    }, that.notificationSeconds() * 1000);
-  }
-
-  this.playNotificationSound = function(){
-    if (that.notificationSoundMode() == "off"){ return; }
-
-    if (that.selectedNotiSound() == "up"){
-      if (that.uploadedSound() == null){ return; }
-
-      var sound_name = that.uploadedSound().replace(".mp3","");
-      ion.sound({
-        sounds: [ { name: sound_name } ],
-        path: "/uploads/",
-        volume: 1
-      });
-
-      ion.sound.play(sound_name);
-    }else{
-      ion.sound({
-        sounds: that.notificationSounds(),
-        path: "/sounds/",
-        volume: 1
-      });
-
-      ion.sound.play(that.selectedNotiSound());
-    }
-  }
 
   this.faviconNumber = new FaviconNumber();
+
+  this.settingViewModel = new SettingViewModel({
+    socket: that.socket
+  });
 
   this.memoController = new MemoController({
     socket: that.socket,
@@ -158,19 +53,26 @@ function ClientViewModel(){
     faviconNumber: that.faviconNumber,
     changedLoginName: function(name){
       that.memoController.setName(name);
-      that.set_avatar();
+      that.settingViewModel.set_avatar();
     },
     showRefPoint: function(id){
       that.memoController.move(id);
     },
-    doNotification: that.doNotification
+    doNotification: that.settingViewModel.doNotification
   });
+
+  this.settingViewModel.loginName.subscribe(function(value){
+    that.chatController.setName(value);
+    that.memoController.setName(value);
+  });
+
 
   this.zenMode = false;
 
   this.init = function(){
     that.init_websocket();
-    that.initSettings();
+
+    that.settingViewModel.init();
 
     if ($(window).width() < 768){
       that.is_mobile = true;
@@ -213,7 +115,7 @@ function ClientViewModel(){
       $('#share_memo_nav').show();
     }
 
-    if ( $.cookie(COOKIE_NAME) == null){
+    if ( that.settingViewModel.loginName() == null){
       setTimeout(function(){
         $('#name_in').modal("show");
         setTimeout(function(){
@@ -221,8 +123,8 @@ function ClientViewModel(){
         },500);
       },500);
     }else{
-      that.chatController.setName($.cookie(COOKIE_NAME));
-      that.memoController.setName($.cookie(COOKIE_NAME));
+      that.chatController.setName(that.settingViewModel.loginName());
+      that.memoController.setName(that.settingViewModel.loginName());
       that.chatController.focus();
     }
 
@@ -274,7 +176,7 @@ function ClientViewModel(){
 
   this.init_websocket = function(){
     that.socket.on('connect', function() {
-      that.set_avatar();
+      that.settingViewModel.set_avatar();
     });
 
     that.socket.on('disconnect', function(){
@@ -282,7 +184,7 @@ function ClientViewModel(){
     });
 
     that.socket.on('set_name', function(name) {
-      that.loginName(name);
+      that.settingViewModel.loginName(name);
     });
   }
 
@@ -326,16 +228,6 @@ function ClientViewModel(){
     $('#settings_modal').modal('show');
   }
 
-  this.login_action = function(){
-    var name = that.loginName();
-
-    if ( name != "" ){
-      that.set_avatar();
-      that.chatController.focus();
-    }
-    $('#name_in').modal('hide')
-  }
-
   this.initSettings = function(){
     var that = this;
 
@@ -345,42 +237,6 @@ function ClientViewModel(){
     }else if (window.localStorage.timeline == 'mention'){
       $('#mention_alert').show();
     }
-
-    // for Send Message Key
-    if(window.localStorage.sendkey == 'ctrl'){
-      $('#send_ctrl').attr('checked', 'checked');
-    }else if (window.localStorage.sendkey == 'shift'){
-      $('#send_shift').attr('checked', 'checked');
-    }else{
-      $('#send_enter').attr('checked', 'checked');
-    }
-
-    $('.send-message-key-radio').on('change', "input", function(){
-      var key = $(this).val();
-      window.localStorage.sendkey = key;
-    });
-  }
-
-  this.upload_avatar = function(){
-    $('#upload_avatar').click();
-    return false;
-  }
-
-  this.set_avatar = function(){
-    $.cookie(COOKIE_NAME, that.loginName(),{ expires: COOKIE_EXPIRES });
-    window.localStorage.avatarImage = that.avatar();
-
-    that.socket.emit('name',
-      {
-        name: that.loginName(),
-        avatar: that.avatar()
-      });
-    return false;
-  }
-
-  this.upload_sound = function(){
-    $('#upload_sound').click();
-    return false;
   }
 
   this.adjust_display_size_for_mobile = function(){
